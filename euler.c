cfafbae6e10671c5ed5913bb5c30ed7e1be5203b
kim1kate <145195952+kim1kate@users.noreply.github.com>
2023-09-16 13:02:31 +0300

    Add files via upload

---
+++ euler.c
@@ -0,0 +1,18 @@
+#include "euler.h"
+
+unsigned long long Problem_1(const int value_, const int* multiplies_ , unsigned int size_)
+{
+   unsigned long long sum = 0;
+   for (unsigned int number = 0; number < value_; ++number)
+   {
+      for (unsigned int i = 0; i < size_; ++i)
+      {
+         if (number % multiplies_[i] == 0)
+         {
+            sum += number;
+            break;
+         }
+      }
+   }
+   return sum;
+}
---
+++ euler.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/*
+* @brief Найдите сумму всех чисел меньше value_ ,кратных multeplies_
+* @param value_:Число меньше которого ищем сумму
+* @param multeplies_ массив кратных чисел.
+* @return Сумма чисел
+*/
+ unsigned long long Problem_1(const int value_, const int* multiplies_,unsigned int size_);
